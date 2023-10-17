2a2673aeadc239e0363e031af8800424902e8eba
slim-jemai <slim_jemai@yahoo.com>
2023-10-17 15:02:54 +0100

    register

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,7 +1,7 @@
 
 export const sample_foods: any[] = [
   {
-    id:'1',
+   
     name: 'Pizza Pepperoni',
     cookTime: '10-20',
     price: 10,
@@ -12,7 +12,7 @@ export const sample_foods: any[] = [
     tags: ['FastFood', 'Pizza', 'Lunch'],
   },
   {
-    id:'2',
+    
     name: 'Meatball',
     price: 20,
     cookTime: '20-30',
@@ -23,7 +23,7 @@ export const sample_foods: any[] = [
     tags: ['SlowFood', 'Lunch'],
   },
   {
-    id:'3',
+    
     name: 'Hamburger',
     price: 5,
     cookTime: '10-15',
@@ -34,7 +34,7 @@ export const sample_foods: any[] = [
     tags: ['FastFood', 'Hamburger'],
   },
   {
-    id:'4',
+    
     name: 'Fried Potatoes',
     price: 2,
     cookTime: '15-20',
@@ -45,7 +45,7 @@ export const sample_foods: any[] = [
     tags: ['FastFood', 'Fry'],
   },
   {
-    id:'5',
+    
     name: 'Chicken Soup',
     price: 11,
     cookTime: '40-50',
@@ -56,7 +56,7 @@ export const sample_foods: any[] = [
     tags: ['SlowFood', 'Soup'],
   },
   {
-    id:'6',
+    
     name: 'Vegetables Pizza',
     price: 9,
     cookTime: '40-50',
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { InputContainerComponent } from './components/partials/input-container/i
 import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
 import { TextInputComponent } from './components/partials/text-input/text-input.component';
 import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { LoadingComponent } from './components/partials/loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,9 @@ import { DefaultButtonComponent } from './components/partials/default-button/def
     InputContainerComponent,
     InputValidationComponent,
     TextInputComponent,
-    DefaultButtonComponent
+    DefaultButtonComponent,
+    RegisterPageComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
